41956c677aa89d187b721987b09c7a5261d93a07
shubham gaur <shubhamgaur672@gmail.com>
2024-01-22 20:38:29 +0530

    adddin selectors

--- src/components/Pages/ChatSuggest.js
+++ src/components/Pages/ChatSuggest.js
@@ -4,11 +4,16 @@ import './ChatSuggest.css'
 import Reply from './Reply';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 function ChatSuggest() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [ask,setAsk]=useState('')
   const [reply,setReply]=useState([])
   const [loading,setLoading]=useState(false)
+  const [tone, setTone] = useState('flirty');
+  const [gender, setGender] = useState('girl');
+  const [lang, setLang] = useState('english');
   
   const handleFileInput = (e) => {
     setSelectedFile(e.target.files[0]);
@@ -22,6 +27,9 @@ function ChatSuggest() {
       const formData = new FormData();
       formData.append('image', selectedFile);
       formData.append('ask', ask);
+      formData.append('gender', gender);
+      formData.append('lang', lang);
+      formData.append('tone', tone);
 
 
       fetch('https://flirty-bear-backend.vercel.app/Chat', {
@@ -97,6 +105,58 @@ const VisuallyHiddenInput = styled('input')({
           
           
            onChange={(event) => setAsk(event.target.value)} ></input>
+            <div className='selector'>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        defaultValue={tone}
+        value={tone}
+        label="tone"
+        color='secondary'
+        onChange={(event) => setTone(event.target.value)}
+        sx={{ m:0.5, minWidth: 50,borderRadius: 4 }} size="small"
+      >
+        
+    
+        <MenuItem value="flirty">Flirty</MenuItem>
+        <MenuItem value="impressive">Impressive</MenuItem>
+        
+        
+      </Select>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        defaultValue={gender}
+        value={gender}
+        label="gender"
+        color='secondary'
+        onChange={(event) => setGender(event.target.value)}
+        sx={{ m:0.5, minWidth: 50,borderRadius: 4  }} size="small"
+      >
+        
+    
+        <MenuItem value="girl"> For Girl</MenuItem>
+        <MenuItem value="boy">For Boy</MenuItem>
+        
+      </Select>
+
+       <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        defaultValue={lang}
+        value={lang}
+        label="Language"
+        color='secondary'
+        onChange={(event) => setLang(event.target.value)}
+        sx={{ m:0.5,  minWidth: 50,borderRadius: 4  }} size="small"
+      >
+        
+    
+        <MenuItem value="english"> English</MenuItem>
+        <MenuItem value="hindi">Hindi</MenuItem>
+        
+      </Select>
+      </div>
         <Button id='generate' type="submit" variant="contained">{loading ? 'Generating...' : `Generate Replies`}</Button>
       </form>
       <div className="caption-container">
